762b4aec845a7ed6efa0fda8d36c72acc1a1e9b5
Yew Long <yewlong42@gmail.com>
2017-08-22 03:22:19 +0000

    shell script best practices

--- monitor.sh
+++ monitor.sh
@@ -1,25 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+#set -x
+
 FS='=FS='
 RS='=RS='
-LOG='/root/stats'
+LOG='/home/ubuntu/dfsg-monitor/stats'
 GZIP_THRESH='10000000'
 INTERVAL=5
 
 while true; do
-        date --iso-8601='seconds' >> $LOG
-        echo $FS >> $LOG
-        uname -a >> $LOG
-        echo $FS >> $LOG
-        netstat -tuan >> $LOG
-        echo $FS >> $LOG
-        vmstat >> $LOG
-        echo $FS >> $LOG
-        free >> $LOG
-        echo $RS >> $LOG
-        size=`stat -c'%s' $LOG`
+        {
+        date --iso-8601='seconds'
+        echo $FS
+        uname -a
+        echo $FS
+        netstat -tuan
+        echo $FS
+        vmstat
+        echo $FS
+        free
+        echo $RS
+        } >> $LOG
+        size=$(stat -c'%s' $LOG)
         if [[ $size -gt $GZIP_THRESH ]]; then
                 gzip $LOG
-                mv "$LOG.gz" "$LOG-`date --iso-8601='seconds'`.gz"
+                mv "$LOG.gz" "$LOG-$(date --iso-8601='seconds').gz"
         fi
         sleep $INTERVAL
 done
