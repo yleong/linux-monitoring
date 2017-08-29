eede3973fb803d14344bf010d7f732d62d694f38
Yew Long <yewlong42@gmail.com>
2017-08-29 10:28:20 +0000

    standardize number of commas in netstat.csv

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -hvP '^Active|^Proto|^procs|swpd|available' $@ | awk -f process.awk | sort | uniq | sed 's/ESTABLISHED/&,/g' | sed 's/^udp.*$/&,/g' > netstat.csv
+grep -hvP '^Active|^Proto|^procs|swpd|available' $@ | awk -f process.awk | sort | uniq | sed 's/ESTABLISHED/&,/g' | sed 's/^udp.*$/&,/g' | sed 's/ESTABLISHED,\+/ESTABLISHED,/g' | sed 's/,,$/, ,/g' > netstat.csv
